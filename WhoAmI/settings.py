a666ad6a6a4aea3caabbfbd37c582a3f12af42f7
Iman <iman.jami@gmail.com>
2014-08-19 17:34:39 +0430

    setting was edited

--- WhoAmI/settings.py
+++ WhoAmI/settings.py
@@ -85,5 +85,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
-TEMPLATE_DIRS = os.path.join(BASE_DIR, 'templates')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
