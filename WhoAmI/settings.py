a765a629694338243daf6f4763efe08a8ab3c538
Iman <iman.jami@gmail.com>
2014-08-19 17:01:30 +0430

    setting was edited

--- WhoAmI/settings.py
+++ WhoAmI/settings.py
@@ -10,6 +10,8 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from django.conf.global_settings import STATICFILES_DIRS, TEMPLATE_DIRS
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -82,3 +84,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+TEMPLATE_DIRS = os.path.join(BASE_DIR, 'templates')
