8c6ea7e90c3ea2ae0b16b7339282e37e2358aba3
Iman <iman.jami@gmail.com>
2014-08-21 17:02:25 +0430

    global settings

--- Chat/views.py
+++ Chat/views.py
@@ -1,5 +1,9 @@
+from httplib import HTTPResponse
 from django.shortcuts import render
 
 # Create your views here.
 def home(request):
     return render(request, 'Chat/chat_test.html', {})
+
+def send_message(request):
+    return HTTPResponse('salam');
