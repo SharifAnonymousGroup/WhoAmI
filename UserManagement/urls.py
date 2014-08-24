918c5050ab2562e3b1803a8103168446906bb023
garfild <hamidkazemi22@gmail.com>
2014-08-24 19:30:56 +0430

    login added to project! "chosi"

--- UserManagement/urls.py
+++ UserManagement/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import patterns, url
+from django.contrib.auth.views import login, logout
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^login/$', 'UserManagement.views.login', name='login'),
+    url(r'^login_request/' , 'UserManagement.views.login_request', name = 'login_request')
+
 )
--- UserManagement/views.py
+++ UserManagement/views.py
@@ -1,7 +1,20 @@
+from django.contrib import auth
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 def login(request):
-    print "salam"
     return render(request, 'test/login_test.html', {})
+
+
+def login_request(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = auth.authenticate(username=username, password=password)
+    if user is not None:
+        print "correct"
+    else:
+        print "not correct"
+        return render(request, 'test/login_test.html', {'error': True})
+    return HttpResponse()
