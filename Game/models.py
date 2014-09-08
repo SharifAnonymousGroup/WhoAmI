304ddbe480948d627b49774ddbb2253d1da6ed42
Milad <milad.ameri73@gmail.com>
2014-09-08 16:25:33 +0430

    models added to Game

--- Game/models.py
+++ Game/models.py
@@ -5,25 +5,38 @@ from UserManagement.models import *
 COLOR_CHOICES = ( ('r', 'red'), ('w', 'white'), ('g', 'green'), ('b', 'blue'), ('o', 'orange'),
                   ('y', 'yellow'), ('p', 'purple'), ('x', 'pink'), ('q', 'grey'))
 
+
 class Player(models.Model):
     member = models.ForeignKey('UserManagement.Member', related_name='player')
-    room = models.ForeignKey('Room', related_name='players')
+    game = models.ForeignKey('Game', related_name='players')
     isAlive = models.BooleanField()
     score = models.IntegerField()
     color = models.CharField(max_length=1, choices=COLOR_CHOICES)
 
-class Room(models.Model):
-    timeOfEachTurn = models.IntegerField() # in second
-    maxNumberOfPlayer = models.IntegerField()
+
+class Game(models.Model):
+    time_of_each_round = models.IntegerField() # in second
+    max_number_of_player = models.IntegerField()
     creator = models.ForeignKey('UserManagement.Member')
-    #url = models.CharField(max_length=10)
+    code = models.CharField(max_length=30)
     turn = models.IntegerField()
+    create_time = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField()
+
 
 class Message(models.Model):
     time = models.DateTimeField(auto_now_add=True)
-    player = models.ForeignKey('Player', related_name='messages')
-    room = models.ForeignKey('Room', related_name='rooms')
+    sender = models.ForeignKey('Player', related_name='messages')
+    round = models.ForeignKey('Round', related_name='messages')
     text = models.TextField(max_length=250)
 
-#class Round(models.Model):
- #   room = models.ForeignKey('Room', related_name='rooms')
+
+class Round(models.Model):
+    game = models.ForeignKey('Game', related_name='games')
+
+
+class Vote(models.Model):
+    round = models.ForeignKey('Round', related_name='votes')
+    voter = models.ForeignKey('Player', related_name='votes')
+    color = models.CharField(choices=COLOR_CHOICES, max_length=1)
+    target = models.ForeignKey('Player', related_name='voted-to', null=True)
