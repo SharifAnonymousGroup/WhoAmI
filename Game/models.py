5e0b8e5da8a53cbb381abf507e172a50908869cc
Milad <milad.ameri73@gmail.com>
2014-09-08 15:17:24 +0430

    salam

--- Game/models.py
+++ Game/models.py
@@ -1,12 +1,9 @@
-from django.db import models
-import datetime
-# Create your models here.
+# Create your models here
+
 from UserManagement.models import *
 
 COLOR_CHOICES = ( ('r', 'red'), ('w', 'white'), ('g', 'green'), ('b', 'blue'), ('o', 'orange'),
                   ('y', 'yellow'), ('p', 'purple'), ('x', 'pink'), ('q', 'grey'))
-
-
 
 class Player(models.Model):
     member = models.ForeignKey('UserManagement.Member', related_name='player')
@@ -15,15 +12,12 @@ class Player(models.Model):
     score = models.IntegerField()
     color = models.CharField(max_length=1, choices=COLOR_CHOICES)
 
-
-
-class Room(models.Model):
+class Game(models.Model):
     timeOfEachTurn = models.IntegerField() # in second
     maxNumberOfPlayer = models.IntegerField()
     creator = models.ForeignKey('UserManagement.Member')
     #url = models.CharField(max_length=10)
     turn = models.IntegerField()
-
 
 class Message(models.Model):
     time = models.DateTimeField(auto_now_add=True)
@@ -31,3 +25,5 @@ class Message(models.Model):
     room = models.ForeignKey('Room', related_name='rooms')
     text = models.TextField(max_length=250)
 
+class Round(models.Model):
+    room = models.ForeignKey('Room', related_name='rooms')
