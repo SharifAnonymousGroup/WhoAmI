0c6553e519e728e144d31260c5dd964d07d4179f
Iman <iman.jami@gmail.com>
2014-09-08 13:56:12 +0430

    start merge changes

--- static/UserManagement/javascripts/homepage.js
+++ static/UserManagement/javascripts/homepage.js
@@ -4,9 +4,9 @@
 
 
 var demoEntered = 0;
-$(document).ready( function() {
+$(document).ready(function () {
 
-    $('#leftside-button').click(function() {
+    $('#leftside-button').click(function () {
 
         $('#left-sidebar').sidebar({
             overlay: false,
@@ -25,10 +25,10 @@ $(document).ready( function() {
             );
             $('#left-sidebar-button-text').css('display', 'none');
 
-            $('#rightside-button').css('marginRight' , '0px' ).animate(
-                {width:"9%"},400
+            $('#rightside-button').css('marginRight', '0px').animate(
+                {width: "9%"}, 400
             );
-            $('#right-sidebar-button-text').css('display','none');
+            $('#right-sidebar-button-text').css('display', 'none');
 
         }
         else {
@@ -38,15 +38,16 @@ $(document).ready( function() {
                     $('#left-sidebar-button-text').css('display', 'inline');
                 }
             );
-            $('#rightside-button').css('marginRight' , '-10px' ).animate(
-                {width:"16%"},400,'swing',function(){
-                    $('#right-sidebar-button-text').css('display','inline');}
+            $('#rightside-button').css('marginRight', '-10px').animate(
+                {width: "16%"}, 400, 'swing', function () {
+                    $('#right-sidebar-button-text').css('display', 'inline');
+                }
             );
 
         }
     });
 
-    $('#rightside-button').click(function(){
+    $('#rightside-button').click(function () {
 
         $('#left-sidebar').sidebar({
             overlay: false,
@@ -65,10 +66,10 @@ $(document).ready( function() {
             );
             $('#left-sidebar-button-text').css('display', 'none');
 
-            $('#rightside-button').css('marginRight' , '0px' ).animate(
-                {width:"9%"},400
+            $('#rightside-button').css('marginRight', '0px').animate(
+                {width: "9%"}, 400
             );
-            $('#right-sidebar-button-text').css('display','none');
+            $('#right-sidebar-button-text').css('display', 'none');
 
         }
         else {
@@ -78,18 +79,109 @@ $(document).ready( function() {
                     $('#left-sidebar-button-text').css('display', 'inline');
                 }
             );
-            $('#rightside-button').css('marginRight' , '-10px' ).animate(
-                {width:"16%"},400,'swing',function(){
-                    $('#right-sidebar-button-text').css('display','inline');}
+            $('#rightside-button').css('marginRight', '-10px').animate(
+                {width: "16%"}, 400, 'swing', function () {
+                    $('#right-sidebar-button-text').css('display', 'inline');
+                }
             );
 
         }
     });
 
     $('.ui.dropdown').dropdown();
-    $('#button-forgotpassword-signin').click(function(){
+    $('#button-forgotpassword-signin').click(function () {
         $('.ui.modal')
             .modal('show')
     });
+
+    $('.ui.form')
+        .form({
+            firstName: {
+                identifier: 'first_name',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Please enter your first name'
+                    }
+                ]
+            },
+            lastName: {
+                identifier: 'last_name',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Please enter your last name'
+                    }
+                ]
+            },
+            email: {
+                identifier: 'email',
+                rules: [
+                    {
+                        type: 'email',
+                        prompt: 'Please enter correct email address'
+                    }
+                ]
+            },
+            username: {
+                identifier: 'username',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Please enter a username'
+                    }
+                ]
+            },
+            age: {
+                identifier: 'age',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Please enter your age'
+                    }
+                ]
+            },
+
+            password: {
+                identifier: 'password',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Please enter a password'
+                    },
+                    {
+                        type: 'length[6]',
+                        prompt: 'Your password must be at least 6 characters'
+                    }
+                ]
+            },
+            confirm_password: {
+                identifier: 'confirm_password',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Please enter your password again'
+                    },
+                    {
+                        type: 'match[password]',
+                        prompt: 'Your password does not match with above password'
+
+                    }
+                ]
+            },
+            terms: {
+                identifier: 'terms',
+                rules: [
+                    {
+                        type: 'checked',
+                        prompt: 'You must agree to the terms and conditions'
+                    }
+                ]
+            }
+        }, {
+            on: 'blur',
+            inline: 'true'
+        });
+
 
 });
