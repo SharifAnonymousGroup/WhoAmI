c6b30b476f71241738f19ea8acce6942716fcafd
po0ya <pooya.mosaddegh72@gmail.com>
2014-10-14 05:32:01 +0000

    syntaxically

--- Game/models.py
+++ Game/models.py
@@ -33,6 +33,7 @@ class Player(models.Model):
     def get_color(self):
         return eval(self.color)[1]
 
+
 class GameManager(models.Manager):
     def create_game(self, name, time_of_each_round, max_number_of_players, creator):
         game = self.model(name=name, time_of_each_round=time_of_each_round,
@@ -126,8 +127,6 @@ class MessageManager(models.Manager):
 
 
 
-
-
 class Message(models.Model):
     sending_time = models.DateTimeField(auto_now_add=True)
     sender = models.ForeignKey('Player', related_name='messages')
@@ -152,8 +151,9 @@ class Round(models.Model):
     current_game = models.ForeignKey('Game', related_name='current_round')
     turn = models.IntegerField()
     objects = RoundManager()
+
     def __unicode__(self):
-        return self.game.__unicode__() + " -> " + self.turn
+        return self.game.__unicode__() + " -> " + str(self.turn)
 
 
 class VoteManager(models.Manager):
