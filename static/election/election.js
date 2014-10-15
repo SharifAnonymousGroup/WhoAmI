e37a79bf23a2e1bcfbdd9036ffa51ef18b48186f
garfild <hamidkazemi22@gmail.com>
2014-10-15 14:55:15 +0330

    room page ui added vote in progress

--- static/election/election.js
+++ static/election/election.js
@@ -7,10 +7,22 @@ $(document).ready(function () {
     var players = JSON.parse($('#json_players').html());
 
 
-
     $('.vote').mouseenter(function () {
         var end = this.value;
         console.log(end);
         console.log("zakhar");
     });
+    $('.ui.dropdown')
+        .dropdown()
+
+    ;
+    $(".vote").dropdown({
+        onChange: function (val) {
+
+            var index = players.indexOf(val);
+            if (index > -1) {
+                array.splice(index, 1);
+            }
+        }
+    })
 });
